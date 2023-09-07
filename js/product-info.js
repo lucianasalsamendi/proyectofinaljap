c31a5fdf282779732d66a301fe37e15ddc9d1040
emiliapalleiro <epalleiro@gmail.com>
2023-09-07 19:21:23 -0300

    cambios comentarios

---
+++ js/product-info.js
@@ -0,0 +1,58 @@
+//comentario
+
+// cambio de estrellas al hacer click en ellas
+var starLabels = document.querySelectorAll('.stars label');
+var selectedRating = document.getElementById('selected-rating');
+var ratingInput = document.getElementById('rating');
+
+starLabels.forEach((label, index) => {
+    label.addEventListener('click', () => {
+        // Cambiar la clase para mostrar la estrella llena o vacía
+        for (let i = 0; i < starLabels.length; i++) {
+            if (i <= index) {
+                starLabels[i].classList.add('fas', 'fa-star-filled');
+                starLabels[i].classList.remove('fa-star-empty');
+            } else {
+                starLabels[i].classList.add('fa-star-empty');
+                starLabels[i].classList.remove('fas', 'fa-star-filled');
+            }
+        }
+
+        // Actualizar el valor del campo de entrada oculto
+        ratingInput.value = index + 1;
+
+        // Actualizar el número de estrellas seleccionadas
+        selectedRating.textContent = index + 1;
+    });
+});
+
+// envío del formulario al servidor
+document.getElementById('comment-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+
+    // Obtener los valores del comentario y la puntuación
+    const comment = document.getElementById('comment').value;
+    const rating = ratingInput.value;
+
+    // 
+    let message = '';
+    if (rating >= 4) {
+        message = '¡Excelente Producto!';
+    } else if (rating >= 3) {
+        message = 'Buen producto';
+    } else if (rating >= 2) {
+        message = 'Aceptable';
+    }
+
+    // Mostrar el mensaje
+    document.getElementById('message').textContent = message;
+   
+    // Limpia el formulario después del envío 
+    document.getElementById('comment').value = '';
+    ratingInput.value = '0';
+    selectedRating.textContent = '0';
+    starLabels.forEach(label => {
+        label.classList.remove('fas');
+        label.classList.add('fa-regular');
+    });
+});
