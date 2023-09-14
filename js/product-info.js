f6de3b3e11bb2e5ab0e3a8c8f3aa67799d901fb3
Bruno Baz <bazbruno13@gmail.com>
2023-09-14 11:48:40 -0300

    soluciono problemas con las estrellas

--- js/product-info.js
+++ js/product-info.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", async () => {
   
   const productID = localStorage.getItem("Product");
@@ -76,29 +77,64 @@ document.addEventListener("DOMContentLoaded", async () => {
       });
     } getProductDetails();
 
-    function getProductcoments(productID) {
-      const comentarios = "https://japceibal.github.io/emercado-api/products_comments/" + productID + ".json";
-      const divComentarios = document.getElementById('product-comment');
-    
-      fetch(comentarios)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('No se pudo obtener los comentarios de la API');
-          }
-          return response.json();
-        })
-        .then(comentarios => {
-          // Manipular los datos obtenidos (en este ejemplo, asumimos que los comentarios son un array)
-          comentarios.forEach(comentario => {
-            const comentarioElement = document.createElement('p');
-            comentarioElement.textContent = comentario.text;
-            divComentarios.appendChild(comentarioElement);
-          });
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }getProductcoments()
-  })
+// cambio de estrellas al hacer click en ellas
+var starLabels = document.querySelectorAll('.stars label');
+var selectedRating = document.getElementById('selected-rating');
+var ratingInput = document.getElementById('rating');
 
-    
+starLabels.forEach((label, index) => {
+    label.addEventListener('click', () => {
+        // Cambiar la clase para mostrar la estrella llena o vacía
+        for (let i = 0; i < starLabels.length; i++) {
+            if (i <= index) {
+                starLabels[i].classList.add('fas', 'fa-star-filled');
+                starLabels[i].classList.remove('fa-star-empty');
+            } else {
+                starLabels[i].classList.add('fa-star-empty');
+                starLabels[i].classList.remove('fas', 'fa-star-filled');
+            }
+        }
+
+        // Actualizar el valor 
+        ratingInput.value = index + 1;
+
+        // Actualizar el número de estrellas seleccionadas
+        selectedRating.textContent = index + 1;
+    });
+});
+
+// envío del formulario al servidor
+document.getElementById('comment-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+
+    // Obtener los valores del comentario y la puntuación
+    var comment = document.getElementById('comment').value;
+    var rating = ratingInput.value;
+
+    // 
+    let message = '';
+    if (rating >= 5) {
+        message = '¡Extraordinario!';
+    }else if (rating >= 4) {
+        message = '¡Excelente Producto!';
+    } else if (rating >= 3) {
+        message = 'Buen producto';
+    } else if (rating >= 2) {
+        message = 'Aceptable';
+    } else if (rating >= 1) {
+        message = 'Malo';
+    }
+
+    // Mostrar el mensaje
+    document.getElementById('message').textContent = message;
+   
+    // Limpia el formulario después del envío 
+    document.getElementById('comment').value = '';
+    ratingInput.value = '0';
+    selectedRating.textContent = '0';
+    starLabels.forEach(label => {
+        label.classList.remove('fas');
+        label.classList.add('fa-regular');
+    });
+})
+})
