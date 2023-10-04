c4f9b191a402df3e72d644bc572613e3c2c2fe9f
Sinho 107 <jose.prestes107@gmail.com>
2023-10-04 20:13:20 -0300

    fetch cart

--- js/cart.js
+++ js/cart.js
@@ -17,20 +17,20 @@ fetch(URL_userID)
                 const cartData = document.getElementById("cart-data");
                 cartData.innerHTML = "";
                 for (const article of articles) {
-                    cartData.innerHTML += `<div class="list-group-item list-group-item-action cursor-active">
-                        <div class="col">
+                    cartData.innerHTML += `<div class="row">
+                        <div class="">
                             <div class ="col-2">
                             <img src="${article.image}" class="img-thumbnail width=1px alt="${article.name}"
                         </div>
-                        <div class ="col-3">
-                            Nombre del artículo: ${article.name}
+                        <div class ="">
+                           ${article.name}
                         </div>
-                        <div class="col-3"
-                            <label for="cantidad">Cantidad: </label>
+                        <div class=""
+                            <label for="cantidad"></label>
                             <input type="number" value="${article.count}" min="1" step="1" id="cantidad">
                         </div>
-                         <div class ="col-3">
-                            Precio unitario: ${article.unitCost} ${article.currency}
+                         <div class ="">
+                            ${article.unitCost} ${article.currency}
                         </div>
                         </div>
                             </div>
