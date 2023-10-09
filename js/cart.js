e5c86ceacba4188e73b20ba7020d5a7774a13dd9
BrunoBazIvan <131315692+BrunoBazIvan@users.noreply.github.com>
2023-10-09 20:01:39 -0300

    abances con el carrito

--- js/cart.js
+++ js/cart.js
@@ -124,3 +124,21 @@ fetch(URL_userID)
   })();
   
   /*Aca termina dark and light*/
+
+  // Ejecutar una función después de 2000 milisegundos (2 segundos)
+setTimeout(function() {
+
+  const carrito = [];
+ // Obtener el elemento con el id 'btnCarrito'
+  const btnCarrito = document.getElementById('btnCarrito');
+
+  // Agregar un evento de clic al elemento
+  btnCarrito.addEventListener('click', () => {
+    Swal.fire(
+      'Felicidades!',
+      'Ha sido agregado al carrito con éxito.',
+      'success'
+    )
+  });
+
+}, 1000);
