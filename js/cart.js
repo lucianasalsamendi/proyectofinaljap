65ee52384295e95ea8f0da0396bb6f838130865b
Sinho 107 <jose.prestes107@gmail.com>
2023-10-09 19:06:44 -0300

    pull

--- js/cart.js
+++ js/cart.js
@@ -18,20 +18,15 @@ fetch(URL_userID)
                 cartData.innerHTML = "";
                 for (const article of articles) {
                     cartData.innerHTML +=
-                    `
+                    `<div class="container text-center">
                     <div class="row">
-                        <div class="">
-                            <div class ="col-2">
-                            <img src="${article.image}" class="img-thumbnail width=1px alt="${article.name}"
-                        </div>
-                        <div class ="">
-                           ${article.name}
-                        </div>
-                        <div class=""
+                        <div class="col-3">
+                        <img src="${article.image}" class="img-thumbnail width=1px alt="${article.name}"</div>
+                        <div class ="col">${article.name}</div>
+                        <div class="col-6"
                             <label for="cantidad"></label>
-                            <input type="number" value="${article.count}" min="1" step="1" id="cantidad">
-                        </div>
-                         <div class ="">
+                            <input type="number" value="${article.count}" min="1" step="1" id="cantidad"></div>
+                         <div class ="col-3">
                             ${article.unitCost} ${article.currency}
                         </div>
                         </div>
