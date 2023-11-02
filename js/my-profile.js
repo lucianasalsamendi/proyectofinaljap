d61153e911f04dd2d59e3f62416496b649354fa1
Facundo Bentancur <140893423+faben79@users.noreply.github.com>
2023-11-01 21:50:48 -0300

    Movimiento al Tabular my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,28 @@
+document.addEventListener("keyindex", function(event) {
+  if (event.key === "Tab") {
+    const campos = Array.from(document.getElementsByTagName("input"));
+    const campoActual = document.activeElement;
+    const currentIndex = campos.indexOf(campoActual);
+
+    if (currentIndex > -1) {
+      let nextIndex;
+      if (event.shiftKey) {
+        // Si se presiona Shift + Tab, navegar hacia la izquierda.
+        nextIndex = currentIndex - 1;
+        if (nextIndex < 0) {
+          nextIndex = campos.length - 1;
+        }
+      } else {
+        // Si se presiona Tab, navegar hacia la derecha.
+        nextIndex = (currentIndex + 1) % campos.length;
+      }
+
+      campos[nextIndex].focus();
+      event.preventDefault();
+    }
+  }
+});
+
 (() => {
     'use strict'
   
