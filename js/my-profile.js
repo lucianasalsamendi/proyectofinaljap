1511e9544e5f0837c99948421b93c86126842701
emiliapalleiro <epalleiro@gmail.com>
2023-11-03 10:15:51 -0300

    arreglando funcionalidad dark and light

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,48 +1,3 @@
-document.addEventListener("keyindex", function(event) {
-  if (event.key === "Tab") {
-    const campos = Array.from(document.getElementsByTagName("input"));
-    const campoActual = document.activeElement;
-    const currentIndex = campos.indexOf(campoActual);
-
-    if (currentIndex > -1) {
-      let nextIndex;
-      if (event.shiftKey) {
-        // Si se presiona Shift + Tab, navegar hacia la izquierda.
-        nextIndex = currentIndex - 1;
-        if (nextIndex < 0) {
-          nextIndex = campos.length - 1;
-        }
-      } else {
-        // Si se presiona Tab, navegar hacia la derecha.
-        nextIndex = (currentIndex + 1) % campos.length;
-      }
-
-      campos[nextIndex].focus();
-      event.preventDefault();
-    }
-  }
-});
-
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
-
-  
 /*! Acá empieza Dark and light
  * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
  * Copyright 2011-2023 The Bootstrap Authors
@@ -124,3 +79,49 @@ document.addEventListener("keyindex", function(event) {
 })();
 
 /*Aca termina dark and light*/
+
+document.addEventListener("keyindex", function(event) {
+  if (event.key === "Tab") {
+    const campos = Array.from(document.getElementsByTagName("input"));
+    const campoActual = document.activeElement;
+    const currentIndex = campos.indexOf(campoActual);
+
+    if (currentIndex > -1) {
+      let nextIndex;
+      if (event.shiftKey) {
+        // Si se presiona Shift + Tab, navegar hacia la izquierda.
+        nextIndex = currentIndex - 1;
+        if (nextIndex < 0) {
+          nextIndex = campos.length - 1;
+        }
+      } else {
+        // Si se presiona Tab, navegar hacia la derecha.
+        nextIndex = (currentIndex + 1) % campos.length;
+      }
+
+      campos[nextIndex].focus();
+      event.preventDefault();
+    }
+  }
+});
+
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+  
